cbee1123d9350ab3374d9ec16720708572e3154a
Basu Kumar <basutulsyan710@gmail.com>
2023-05-08 19:06:54 +0530

    index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+let textbox=document.getElementById("textbox");
+
+textbox.addEventListener('input',function(){
+    let text=this.value;
+    document.getElementById("char").innerHTML=text.length;
+
+    text=text.trim(); // removes extra space from starting and end
+
+    let words=text.split(" "); //splits text after each " " and puts in an array
+    let cleanList=words.filter(function(elm){
+        return elm!="";
+    })
+    document.getElementById("word").innerHTML=cleanList.length;
+})
