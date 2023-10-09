56a0de592b80bd9168f6c8aa113f858f7da4c002
Basu Kumar <basutulsyan710@gmail.com>
2023-10-09 12:32:42 +0530

    arrow function used

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let textbox=document.getElementById("textbox");
+let textbox = document.getElementById("textbox");
 
-textbox.addEventListener('input',function(){
-    let text=this.value;
-    document.getElementById("char").innerHTML=text.length;
+textbox.addEventListener("input", function () {
+  let text = this.value;
+  document.getElementById("char").innerHTML = text.length;
 
-    text=text.trim(); // removes extra space from starting and end
+  text = text.trim(); // removes extra space from starting and end
+  let words = text.split(" "); //splits text after each " " and puts in an array
 
-    let words=text.split(" "); //splits text after each " " and puts in an array
-    let cleanList=words.filter(function(elm){
-        return elm!="";
-    })
-    document.getElementById("word").innerHTML=cleanList.length;
-})
+  let cleanList = words.filter((elm) => elm != "");
+  document.getElementById("word").innerHTML = cleanList.length;
+});
